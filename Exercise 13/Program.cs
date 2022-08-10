ca808e0e3c63068e215f3bb4c48b52ec113162d8
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-10 15:01:23 +0300

    Add ex 13

---
+++ Exercise 13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if((num / 100) % 10 == 0)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"{(num / 100) % 10}");
+}
+
