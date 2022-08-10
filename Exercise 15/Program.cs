54ae066ec9894b896a074cb11f65353ae8c72904
Nargiz Ibragimova <nargizibragimova@MacBook-Pro-Nargiz-3.local>
2022-08-10 15:06:58 +0300

    Add ex 15

---
+++ Exercise 15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int N = int.Parse(Console.ReadLine());
+if((N == 6) || (N == 7))
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
